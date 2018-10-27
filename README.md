fe20419ca4326a6dea41bfdf6125afe0b537fc25
Rinigus <rinigus.git@gmail.com>
2018-10-27 23:01:22 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# qmlrunner
-Runner for QML-only apps
+# QML Runner
+
+Runner for QML-only apps. It runs applications assuming that they are installed
+in the given prefix (`/usr/share`) by default with the following convention
+
+```
+PREFIX/app-name/qml/app-name.qml
+```
+
+Note that the QML file is loaded assuming that its a window. Don't forget to setup
+it to be visible by setting `visible: true` property.
---
+++ src/main.cpp
@@ -0,0 +1,86 @@
+/*
+ * Copyright (C) 2018 Rinigus https://github.com/rinigus
+ *
+ * This file is part of QML Runner.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <QDir>
+#include <QFileInfo>
+#include <QGuiApplication>
+#include <QStringList>
+#include <QQmlApplicationEngine>
+#include <QQuickWindow>
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+  QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+  QGuiApplication app(argc, argv);
+  
+  const QStringList arguments = QCoreApplication::arguments();
+  QString appName;
+  QString prefix = "/usr/share";
+  QStringList paths;
+  for (int i = 1, size = arguments.size(); i < size; ++i)
+    if (!arguments.at(i).startsWith(QLatin1Char('-')))
+      appName = arguments.at(i);
+    else if (arguments.at(i) == "-P" && i+1 < size)
+      {
+        prefix = arguments.at(i+1);
+        ++i;
+      }
+    else if (arguments.at(i) == "-plugin" && i+1 < size)
+      {
+        paths.push_back(arguments.at(i+1));
+        ++i;
+      }
+
+  if (appName.isEmpty())
+    {
+      std::cerr << "Usage: " << argv[0] << " [-P prefix] [-plugin path] appname\n";
+      std::cerr << "Loaded QML is determined by prefix and appname as follows:\n"
+                << " prefix/appname/qml/appname.qml\n"
+                << "by default, prefix is /usr/share\n";
+      return -1;
+    }
+  
+  QDir dir(prefix);
+  QString qmlpath = dir.absoluteFilePath(appName + "/qml/" + appName + ".qml");
+  if (!QFileInfo::exists(qmlpath))
+    {
+      std::cerr << "Cannot find " << qmlpath.toStdString() << "\n";
+      return -2;
+    }
+
+  app.setApplicationName(appName);
+  app.setOrganizationName(appName);
+  app.setApplicationVersion("1.0");
+
+  QQmlApplicationEngine engine;
+  for (const auto &p: paths)
+    engine.addImportPath(p);
+
+  engine.load(qmlpath);
+  if (engine.rootObjects().isEmpty())
+    {
+      std::cerr << "Error loading " << qmlpath.toStdString() << "\n";
+      return -3;
+    }
+
+  return app.exec();
+}
